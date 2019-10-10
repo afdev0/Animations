62435f5c48a1771d022416c71a206e4d0a300e4d
Ariel Fertman <arielfertman@gmail.com>
2019-10-09 20:32:29 -0700

    Initial Commit

---
+++ Animations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animations
+//
+//  Created by Ariel Fertman on 10/9/19.
+//  Copyright © 2019 Ariel Fertman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
